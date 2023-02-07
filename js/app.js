455f1826173ac796fdeeee055180bc887daf138a
rennavittorio <renna.vittorio@gmail.com>
2023-02-07 10:38:34 +0100

    REFACTOR index control for arrows function refactored + code cleaned from excessive comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,5 @@
-// console.log('lesss carousel');
-
-// MLS 1 e 2
-// recupero le slide dal DOM
-// const slideElements = document.getElementsByClassName('slide');
-// console.log(slideElements);
-
-
-// MLS 3
-// creao una lista con i src delle img
+//START MLS 3
+//Creo elementi in DOM partendo da una lista di img
 let imgSrcList = [
     './assets/one.png',
     './assets/two.png',
@@ -16,221 +8,68 @@ let imgSrcList = [
     './assets/five.png'
 ];
 
-// recupero elemento carousel
 let carouselElement = document.querySelector('.carousel');
-// console.log(carouselElement);
 
-//creo gli elementi da inserire nel DOM
 for (i = 0; i < imgSrcList.length; i++){
-    //creo class
+
+    //mi serve la classe active solo nella prima slide che creo
     let addClassActive = ' ';
-    
-    //se primo elemento-active
     if (i === 0){
         addClassActive = ' active';
     };
-    
-    //creo elemento
+
     let currentImg = `
     <div class="slide` + addClassActive + `">
         <img src="${imgSrcList[i]}" alt="">
     </div>
     `;
 
-    
-    //inserisco elemento in DOM
     carouselElement.innerHTML += currentImg;
     
-}
+};
 
-console.log(carouselElement.innerHTML)
-
-// for (i = 0; i < imgSrcList.length; i++){
-//     //creo elemento
-//     let currentImg = ``;
-//     //se primo elemento-active
-//     if (i === 0){
-//         currentImg = `
-//         <div class="slide active">
-//         <img src="${imgSrcList[i]}" alt="">
-//         </div>
-//         `;
-        
-//     } else {
-//         currentImg = `
-//         <div class="slide">
-//         <img src="${imgSrcList[i]}" alt="">
-//         </div>
-//         `;
-//     }
-    
-//     //inserisco elemento in DOM
-//     carouselElement.innerHTML += currentImg;
-    
-// }
-
-//recupero le slide create nel DOM
 let slideElements = document.querySelectorAll('.slide');
-// console.log(slideElements);
-
-
-//recupero le frecce dal DOM
 const rightBtnElement = document.querySelector('.arrow-right');
 const leftBtnElement = document.querySelector('.arrow-left');
 
-//setto l'index counter
+//setto un indice per controllare gli eventi delle frecce
 let indexActiveSlide = 0;
 
-// BONUS - MLS 4
-//richiamo l'evento click sulla freccia dx
 rightBtnElement.addEventListener('click', function(){
     
+    let currentSlide = slideElements[indexActiveSlide];
+    currentSlide.classList.remove('active');
+
+    //controllo l'indice per creare uno scroll infinito
     if (indexActiveSlide < slideElements.length - 1){
-        //elimino la classe active dall'elemento corrente
-        let currentSlide = slideElements[indexActiveSlide];
-        currentSlide.classList.remove('active');
-    
-        //incremento l'indice della slide
         indexActiveSlide +=1;
     
-        //aggiungo class active alla slide successiva
-        let nextSlide = slideElements[indexActiveSlide]; 
-        nextSlide.classList.add('active');
-    
-        console.log(indexActiveSlide); //mostro index in console
-
     } else {
-        let currentSlide = slideElements[indexActiveSlide];
-        currentSlide.classList.remove('active');
-    
-        //RIPRISTINO INDEX AL PRIMO ELEMENTO
         indexActiveSlide = 0;
-    
-        //aggiungo class active alla slide successiva
-        let nextSlide = slideElements[indexActiveSlide]; 
-        nextSlide.classList.add('active');
-    
-        console.log(indexActiveSlide); //mostro index in console
     }
+    
+    let nextSlide = slideElements[indexActiveSlide]; 
+    nextSlide.classList.add('active');
 
-})
+    console.log(indexActiveSlide);
+});
 
-//richiamo l'evento click sulla freccia sx
+
 leftBtnElement.addEventListener('click', function(){
-    
+
+    let currentSlide = slideElements[indexActiveSlide];
+    currentSlide.classList.remove('active');
+
     if (indexActiveSlide > 0){
-        //elimino la classe active dall'elemento corrente
-        let currentSlide = slideElements[indexActiveSlide];
-        currentSlide.classList.remove('active');
-    
-        //decremento l'indice della slide
         indexActiveSlide -=1;
-    
-        //aggiungo class active alla slide precedente
-        let nextSlide = slideElements[indexActiveSlide]; 
-        nextSlide.classList.add('active');
-    
-        console.log(indexActiveSlide); //mostro index in console
-
     } else {
-        //elimino la classe active dall'elemento corrente
-        let currentSlide = slideElements[indexActiveSlide];
-        currentSlide.classList.remove('active');
-    
-        //RIPRISTINO INDEX ALL'ULTIMO ELEMENTO (LENGTH - 1)
         indexActiveSlide = slideElements.length - 1;
-    
-        //aggiungo class active alla slide precedente
-        let nextSlide = slideElements[indexActiveSlide]; 
-        nextSlide.classList.add('active');
-    
-        console.log(indexActiveSlide); //mostro index in console
     }
+    
+    let nextSlide = slideElements[indexActiveSlide]; 
+    nextSlide.classList.add('active');
 
-})
+    console.log(indexActiveSlide);
+});
 
-// MLS 3
-//richiamo l'evento click sulla freccia dx
-// rightBtnElement.addEventListener('click', function(){
-    
-//     if (indexActiveSlide < slideElements.length - 1){
-//         //elimino la classe active dall'elemento corrente
-//         let currentSlide = slideElements[indexActiveSlide];
-//         currentSlide.classList.remove('active');
-    
-//         //incremento l'indice della slide
-//         indexActiveSlide +=1;
-    
-//         //aggiungo class active alla slide successiva
-//         let nextSlide = slideElements[indexActiveSlide]; 
-//         nextSlide.classList.add('active');
-    
-//         console.log(indexActiveSlide); //mostro index in console
 
-//     }
-
-// })
-
-// //richiamo l'evento click sulla freccia sx
-// leftBtnElement.addEventListener('click', function(){
-    
-//     if (indexActiveSlide > 0){
-//         //elimino la classe active dall'elemento corrente
-//         let currentSlide = slideElements[indexActiveSlide];
-//         currentSlide.classList.remove('active');
-    
-//         //decremento l'indice della slide
-//         indexActiveSlide -=1;
-    
-//         //aggiungo class active alla slide precedente
-//         let nextSlide = slideElements[indexActiveSlide]; 
-//         nextSlide.classList.add('active');
-    
-//         console.log(indexActiveSlide); //mostro index in console
-
-//     }
-
-// })
-
-// MLS 1 e 2
-//richiamo l'evento click sulla freccia dx
-// rightBtnElement.addEventListener('click', function(){
-    
-//     if (indexActiveSlide < slideElements.length - 1){
-//         //elimino la classe active dall'elemento corrente
-//         let currentSlide = slideElements[indexActiveSlide];
-//         currentSlide.classList.remove('active');
-    
-//         // incremento l'indice della slide
-//         indexActiveSlide +=1;
-    
-//         //aggiungo class active alla slide successiva
-//         let nextSlide = slideElements[indexActiveSlide]; 
-//         nextSlide.classList.add('active');
-    
-//         console.log(indexActiveSlide); // mostro index in console
-
-//     }
-
-// })
-
-// //richiamo l'evento click sulla freccia sx
-// leftBtnElement.addEventListener('click', function(){
-    
-//     if (indexActiveSlide > 0){
-//         //elimino la classe active dall'elemento corrente
-//         let currentSlide = slideElements[indexActiveSlide];
-//         currentSlide.classList.remove('active');
-    
-//         // decremento l'indice della slide
-//         indexActiveSlide -=1;
-    
-//         //aggiungo class active alla slide precedente
-//         let nextSlide = slideElements[indexActiveSlide]; 
-//         nextSlide.classList.add('active');
-    
-//         console.log(indexActiveSlide); // mostro index in console
-
-//     }
-
-// })
